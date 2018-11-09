b24f38b8c11e6aa564904039ae23c93b130171ad
Connor Peebles <connorpeebles93@gmail.com>
2018-11-09 20:09:35 +0000

    updated create controller for Review so that it will save the review (if valid) and redirect to the same page, else render the same page and display error message

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,26 @@
 class ReviewsController < ApplicationController
+
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @review.user = current_user
+
+    @product = Product.find params[:product_id]
+    @review.product = @product
+    
+    if @review.save
+      redirect_to product_path(params[:product_id])
+    else
+      render 'products/show'
+    end
   end
+
+  private
+
+  def review_params
+    params.require(:review).permit(
+      :rating,
+      :description
+    )
+  end
+
 end
