3d2eeaa77c24555e63139dba4eed59b6d7f6c806
Connor Peebles <connorpeebles93@gmail.com>
2018-11-09 14:08:10 +0000

    added raletionships of reviews to the product and user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
+  has_many :reviews
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
