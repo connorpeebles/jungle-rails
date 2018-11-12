4213733a34d9769fe78b7b90f4c810cd287ad593
Connor Peebles <connorpeebles93@gmail.com>
2018-11-12 18:19:55 +0000

    implemented a minimum password length (of 3) and wrote the test for the validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password, presence: true, length: { minimum: 3 }
   validates :password_confirmation, presence: true
   
   has_secure_password
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
     subject {
-      described_class.new(first_name: "First", last_name: "Last", email: "test@example.com", password: "password", password_confirmation: "password")
+      described_class.new(first_name: "First", last_name: "Last", email: "test@example.com", password: "abc", password_confirmation: "abc")
     }
 
     it 'is valid with attributes first_name, last_name, email, password, and password_confirmation' do
@@ -31,17 +31,22 @@ RSpec.describe User, type: :model do
       expect(subject).to_not be_valid
     end
     it 'is not valid if password_confirmation does not match password' do
-      subject.password_confirmation = "Password"
+      subject.password_confirmation = "ABC"
       expect(subject).to_not be_valid
     end
     it 'is not valid if email already in database' do
-      exUser = User.new(first_name: "exFirst", last_name: "exLast", email: "test@example.com", password: "exPassword", password_confirmation: "exPassword")
+      exUser = User.new(first_name: "exFirst", last_name: "exLast", email: "test@example.com", password: "abc", password_confirmation: "abc")
       exUser.save
       expect(subject).to_not be_valid
     end
     it 'is not valid if email already in database, but different casing' do
-      exUser = User.new(first_name: "exFirst", last_name: "exLast", email: "TEST@example.com", password: "exPassword", password_confirmation: "exPassword")
+      exUser = User.new(first_name: "exFirst", last_name: "exLast", email: "TEST@example.com", password: "abc", password_confirmation: "abc")
       exUser.save
+      expect(subject).to_not be_valid
+    end
+    it 'is not valid if password is less than 3 characters' do
+      subject.password = "ab"
+      subject.password_confirmation = "ab"
       expect(subject).to_not be_valid
     end
   end
